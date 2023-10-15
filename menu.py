b7a16dae5679d1efb2f61a3dfa443ea260c19061
Jack Hudson <103269186+jackhudsonn@users.noreply.github.com>
2023-10-15 12:31:13 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,31 +1,44 @@
-from Tools import *
+    from Tools import *
 from importlib import import_module
 from sys import modules
-import subprocess
+
+
+def terminalGameHub() -> str:
+    result = ""
+    result += "  _____ _____ ____  __  __ ___ _   _    _    _ \n"
+    result += " |_   _| ____|  _ \\|  \\/  |_ _| \\ | |  / \\  | |\n"
+    result += "   | | |  _| | |_) | |\\/| || ||  \\| | / _ \\ | |\n"
+    result += "   | | | |___|  _ <| |  | || || |\\  |/ ___ \\| |___\n"
+    result += "   |_| |_____|_| \\_\\_|  |_|___|_| \\_/_/   \\_\\_____|\n"
+    result += "   ____    _    __  __ _____     _   _ _   _ ____\n"
+    result += "  / ___|  / \\  |  \\/  | ____|   | | | | | | | __ )\n"
+    result += " | |  _  / _ \\ | |\\/| |  _|     | |_| | | | |  _ \\\n"
+    result += " | |_| |/ ___ \\| |  | | |___    |  _  | |_| | |_) |\n"
+    result += "  \\____/_/   \\_\\_|  |_|_____|   |_| |_|\\___/|____/\n\n"
+    return result
 
 
 if __name__ == "__main__":
-    print("  _____ _____ ____  __  __ ___ _   _    _    _ ")
-    print(" |_   _| ____|  _ \\|  \\/  |_ _| \\ | |  / \\  | |")
-    print("   | | |  _| | |_) | |\\/| || ||  \\| | / _ \\ | |")
-    print("   | | | |___|  _ <| |  | || || |\\  |/ ___ \\| |___")
-    print("   |_| |_____|_| \\_\\_|  |_|___|_| \\_/_/   \\_\\_____|")
-    print("   ____    _    __  __ _____     _   _ _   _ ____")
-    print("  / ___|  / \\  |  \\/  | ____|   | | | | | | | __ )")
-    print(" | |  _  / _ \\ | |\\/| |  _|     | |_| | | | |  _ \\")
-    print(" | |_| |/ ___ \\| |  | | |___    |  _  | |_| | |_) |")
-    print("  \\____/_/   \\_\\_|  |_|_____|   |_| |_|\\___/|____/\n")
-
     ext = 'n'
     while ext == 'n':
-        username = choice(open("users.txt").read().split(),
-                          "ENTER A REGISTERED GITHUB USERNAME:\n",
-                          "PLEASE RE-ENTER A VALID USERNAME\n"
-                          "TO CHECK IF THIS IS A REGISTERED USERNAME, CHECK 'users.txt':\n")
-        game = choice(open(f"{username}_games.txt").read().split(),
-                      f"ENTER THE GAME BY {username} THAT YOU WOULD LIKE TO PLAY:\n",
-                      "PLEASE RE-ENTER A VALID GAME\n"
-                      f"TO CHECK IF THIS IS A REGISTERED GAME, CHECK '{username}_games.txt':\n")
+        username = ''
+        game = 'b'
+        while game == 'b':
+            switchScreen()
+            username = choice(open("users.txt").read().split(),
+                              terminalGameHub() + "ENTER A REGISTERED GITHUB USERNAME: ",
+                              terminalGameHub() + "PLEASE RE-ENTER A VALID USERNAME\n"
+                              "TO CHECK IF THIS IS A REGISTERED USERNAME, CHECK 'users.txt': ",
+                              switch_on_fail=True)
+            switchScreen()
+            games = open(f"{username}_games.txt").read().split()
+            games.append('b')
+            game = choice(games, f"GAME: ", "PLEASE RE-ENTER A VALID GAME OR PRESS 'b' TO GO BACK: ",
+                          list_options=True,
+                          pre_list_text=terminalGameHub()+f"ENTER THE GAME BY {username} "
+                                                          f"THAT YOU WOULD LIKE TO PLAY OR PRESS 'b' TO GO BACK: ",
+                          switch_on_fail=True)
+
         file_name = username + '_' + game
 
         switchScreen()
